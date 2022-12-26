ed7d23286944ab8f037daf3765e3377463c645a7
anvin ps <anvin384@gmail.com>
2022-12-26 20:48:05 +0530

    styels added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { AppBar, Container, Typography } from '@material-ui/core';
+import { AppBar, Container, Grid, Grow, Typography } from '@material-ui/core';
 
 import memories from './images/memories.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="120" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
