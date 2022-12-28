20e7cb4798ed99b630c258ae2b9a729f7d9af7fe
anvin ps <anvin384@gmail.com>
2022-12-28 20:59:07 +0530

    list created  posts

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ function App() {
             <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={5}>
               <Form />
             </Grid>
           </Grid>
--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Box,
+} from '@material-ui/core/';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import useStyles from './styles';
+import moment from 'moment';
 
-const Post = () => {
+const Post = ({
+  post: {
+    _id,
+    selectedFile,
+    title,
+    creator,
+    createdAt,
+    tags,
+    message,
+    likeCount,
+  },
+}) => {
   const classes = useStyles();
-  return <div>Post</div>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia className={classes.media} image={selectedFile} title={title} />
+      <Box className={classes.overlay}>
+        <Typography variant="h6">{creator}</Typography>
+        <Typography variant="body">{moment(createdAt).fromNow()}</Typography>
+      </Box>
+      <Box className={classes.overlay2}>
+        <Button style={{ color: 'white' }} size="small">
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </Box>
+      <Box className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {tags.map((tag) => `#${tag}`)}
+        </Typography>
+      </Box>
+      <CardContent>
+        <Box className={classes.title}>
+          <Typography variant="h5" gutterBottom>
+            {message}
+          </Typography>
+        </Box>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary">
+          <ThumbUpAltIcon fontSize="small" /> Like {likeCount}
+        </Button>
+        <Button size="small" color="primary">
+          Delete <DeleteIcon fontSize="small" />
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
