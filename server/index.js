4d419dab1ff09283ad7b0aecac9234a1a0076f48
anvin ps <anvin384@gmail.com>
2023-01-30 22:02:57 +0530

    environmental variable for server file

--- server/index.js
+++ server/index.js
@@ -2,23 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/post.js';
 
 const app = express();
+ dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const DB_USER = 'anvin';
-const DB_PSW = 'samsung123';
-const CONNECTION_URL = `mongodb://${DB_USER}:${DB_PSW}@localhost:27017/mern_stack_socialmedia`;
+
 const PORT = process.env.PORT || 5000;
 mongoose.set('strictQuery', false);
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
